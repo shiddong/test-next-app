ed44f421d41c467651d7c4453cd9dcbad8e1dca8
Richard Shih <shiddong0724@gmail.com>
2023-11-03 10:40:24 +0800

    chore: 修改文案

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import { PropsWithChildren, ReactNode } from "react";
 export default function DashboartLayout({ children }: PropsWithChildren) {
   return (
     <div>
-      Dashboard Layout
+      Dashboard Layout Wrapper
       {/* Include shared UI here e.g. a header or sidebar */}
       <section>{children}</section>
     </div>
